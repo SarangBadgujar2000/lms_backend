e8d072b780586232d14123378edd8d972ad7e3aa
Sarang <sarangrajendrabadgujar@gmail.com>
2023-11-05 12:31:34 +0530

    updated package-lock.json

--- constants/validator.js
+++
@@ -1,9 +0,0 @@
-const emailValidator=(email)=>{
-    const regex=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-    if(regex.test(email)){
-        return true;
-    }
-    return false;
-}
-
-module.exports=emailValidator
